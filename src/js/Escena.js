b0e128fc6d56818408b4c6a14fef7985f1989868
Guido Rombolá <guidorombola@yahoo.com.ar>
2019-09-25 15:08:36 -0300

    Redimensiona graficos al cambiar tamanio de ventana sin hacer click en escena

--- src/js/Escena.js
+++ src/js/Escena.js
@@ -35,7 +35,14 @@ export function comenzarEscena(){
 
   scene.add(ambient);
 
-  var winresize = new WindowResize(renderer, camera);
+  window.addEventListener('resize', onResize, false);
+
+  function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+  }
 
   // Rotation around the center
   var controls;
